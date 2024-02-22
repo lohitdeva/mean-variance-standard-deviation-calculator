282d6dde4c1495b087dfd923f4b6a9f866d0dde4
Lohit Deva <devaboi8@gmail.com>
2024-02-22 11:43:43 +0000

    Completed the project

--- mean_var_std.py
+++ mean_var_std.py
@@ -2,7 +2,54 @@ import numpy as np
 
 def calculate(list):
 
+    if len(list) != 9:
+        raise ValueError("List must contain nine numbers.")
+        
+    new_array = np.array([list[0:3], list[3:6], list[6:]])
 
+    means = [
+        np.mean(new_array, axis=0).tolist(),
+        np.mean(new_array, axis=1).tolist(),
+        np.mean(new_array)
+    ]
 
+    variances = [
+        np.var(new_array, axis=0).tolist(),
+        np.var(new_array, axis=1).tolist(),
+        np.var(new_array)
+    ]
+
+    std_devs = [
+        np.std(new_array, axis=0).tolist(),
+        np.std(new_array, axis=1).tolist(),
+        np.std(new_array)
+    ]
+
+    maxs = [
+        np.max(new_array, axis=0).tolist(),
+        np.max(new_array, axis=1).tolist(),
+        np.max(new_array)
+    ]
+
+    mins = [
+        np.min(new_array, axis=0).tolist(),
+        np.min(new_array, axis=1).tolist(),
+        np.min(new_array)
+    ]
+
+    sums = [
+        np.sum(new_array, axis=0).tolist(),
+        np.sum(new_array, axis=1).tolist(),
+        np.sum(new_array)
+    ]
+
+    calculations = {
+        'mean': means,
+        'variance': variances,
+        'standard deviation': std_devs,
+        'min': mins,
+        'max': maxs,
+        'sum': sums
+    }
 
     return calculations
